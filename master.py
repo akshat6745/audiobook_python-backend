8ff7f9a14042e41cf87657189bf377010fbd4994
akshat6745 <akshat.aggarwal6745>
2025-03-19 23:47:42 +0530

    final changes

--- master.py
+++ master.py
@@ -76,4 +76,4 @@ def get_epub_chapters(filename):
     return jsonify(parse_epub(file_path))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
